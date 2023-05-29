412514271b329610b002461a3df653ff00ee0664
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-05-29 12:27:31 -0400

    a3 lab edits

--- src/App.js
+++ src/App.js
@@ -7,21 +7,17 @@ import {Routes, Route} from "react-router";
 function App() {
     
    return (
-    <BrowserRouter>
-      <div className="container">
-               <Routes>
-          {/* <Route path="/labs"
-                 element={<Labs/>}/> */}
-                    <Route index
-                 element={<Labs/>}/>
-          <Route path="/hello"
-                 element={<HelloWorld/>}/>
-          <Route path="/tuiter/*"
-                 element={<Tuiter/>}/>
-        </Routes>
-
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+       <Route path="/"         element={<Navigate to="/labs/a3"/>}/>
+           <Route path="/labs/*"   element={<Labs/>}/>
+  
+            <Route path="/hello" element={<HelloWorld />} />
+            <Route path="/tuiter/*" element={<Tuiter />} />
+          </Routes>
+        </div>
       </BrowserRouter>
-   );
+    );
 }
 export default App;
