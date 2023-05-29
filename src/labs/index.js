5a2dff5332294a7970878e21a01c0898914b1db6
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-05-29 12:25:28 -0400

    a3 lab edits

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,14 +1,16 @@
 import Assignment3 from "./a3";
 import Nav from "../nav";
+import {Routes, Route}
+  from "react-router";
 function Labs() {
- return (
-   <div>
-       <Nav/>
-       {/* <Link to="/">Lab</Link> |
-   <Link to="/hello">Hello</Link> |
-   <Link to="/tuiter">Tuiter</Link> */}
-     <Assignment3/>
-   </div>
- );
+  return (
+    <div>
+      <Nav />
+       <Routes>
+       <Route path="a3"
+        element={<Assignment3/>}/>
+ </Routes>
+    </div>
+  );
 }
 export default Labs;
