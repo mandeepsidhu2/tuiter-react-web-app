8d273a203d4b63519e407eba82cc7a08211106d8
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-05-29 12:44:49 -0400

    a3 lab edits

--- src/labs/a3/working-with-arrays.js
+++ src/labs/a3/working-with-arrays.js
@@ -1,3 +1,12 @@
+import ArrayIndexAndLength from "./array-index-and-length";
+import AddingAndRemovingDataToFromArrays from "./adding-and-removing-data-to-from-arrays"
+import ForLoops from './for-loops'
+import MapFunction from './map-function'
+import FindFunction from "./find-function";
+import FilterFunction from "./filter-function";
+import JsonStringify from "./json-stringify";
+import FindIndex from "./find-index";
+
 function WorkingWithArrays() {
     let numberArray1 = [2, 3, 4, 5];
     let stringArray1 = ['string1', 'string2'];
@@ -20,7 +29,14 @@ function WorkingWithArrays() {
             numberArray1 = {numberArray1}<br/>
             stringArray1 = {stringArray1}<br/>
             variableArray1 = {variableArray1}<br/>
-
+            <ArrayIndexAndLength />
+            <AddingAndRemovingDataToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <JsonStringify />
+            <FindFunction />
+            <FindIndex />
+            <FilterFunction />
         </div>
        
     );
